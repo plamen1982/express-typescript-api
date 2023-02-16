204e0417ea8b87159ba512872dd1ef416f554130
plamen1982 <plamen.hristov@ymail.com>
2023-02-16 13:32:43 +0200

    1. article for Promises(MDN), 2. added in package.json prism method for the seed.ts file, 3. creating seed.ts file[C

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #### theOriginalVideo https://www.youtube.com/watch?v=PM58NEMJgMw
 #### https://plainenglish.io/blog/typescript-and-global-variables-in-node-js-59c4bf40cb31 - TYPESCRIPT GLOBALS ARTICLE
+#### https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise - Promises Explained
---
+++ prisma/seed.ts
@@ -0,0 +1,109 @@
+import { db } from  "../src/utils/db.server";
+
+type Author = {
+    firstName: string;
+    lastName: string;
+}
+
+type Book = {
+    title: string;
+    isFiction: boolean;
+    datePublished: Date;
+}
+
+/**
+ * @function 
+ * @name seed
+ * @description populate the database(Author and Book) with some dummy data, and return some record for testing purposes
+ */
+
+async function seed() {
+
+    await Promise.all(
+        getAuthors().map((author) => {
+            /**
+             *  for every author return from our seed function connect with the database with the db(PrismClient)
+             *  when connected seed the data object(he will not be considered as a field from the database)
+             *  create these records in this case in Author table in the database
+             */
+            return db.author.create({
+                data: {
+                    firstName: author.firstName,
+                    lastName: author.lastName
+                }
+            })
+        })
+    )
+
+    await Promise.all(
+        getBooks().map((book) => {
+            const { title, isFiction, datePublished } = book;
+            return db.book.create({
+                data: {
+                    title,
+                    isFiction,
+                    datePublished,
+                    authorId: author.id
+                }
+            })
+        })
+    )
+    /**
+     * @returns single record from the database of author, by single criteria
+     */
+
+    const author = await db.author.findFirst({
+        where: {
+            firstName: "Yuval Noah"
+        }
+    })
+}
+
+/**
+ * @function
+ * @name getBooks
+ * @returns Array of objects from type Book
+ */
+
+function getBooks(): Array<Book> {
+    return [
+        {
+            title: "Sapiens",
+            isFiction: false,
+            datePublished: new Date()
+        },
+        {
+            title: "Homo Deus",
+            isFiction: false,
+            datePublished: new Date()
+        },
+        {
+            title: "The Ugly Duckling",
+            isFiction: false,
+            datePublished: new Date()
+        }
+    ]
+}
+
+/**
+ * @function
+ * @name getAuthors
+ * @returns Array of objects from type Author
+ */
+
+function getAuthors(): Array<Author> {
+    return [
+        {
+            firstName: "John",
+            lastName: "Doe"
+        },
+        {
+            firstName: "William",
+            lastName: "Shakespeare"
+        },
+        {
+            firstName: "Yuval Noah",
+            lastName: "Harari"
+        }
+    ]
+}
